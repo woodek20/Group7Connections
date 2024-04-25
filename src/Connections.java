b56091890052a45cb70dce971db34fb5267ff93a
hessac20 <hessac20@wfu.edu>
2024-04-25 12:44:31 -0400

    Figuring out Push and Pull

---
+++ src/Connections.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Connections {
+
+    private ArrayList<String> words;
+
+    public static void main(String[] args) {
+
+    }
+
+    public ArrayList WordGroups(){
+        words = new ArrayList<>();
+        return words;
+    }
+
+}
